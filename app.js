f6976f58c805b8807cec6713942c3c9201414083
sajjmon <86625235+sajjmon@users.noreply.github.com>
2021-09-04 22:28:41 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ window.addEventListener('load', () => {
 
     let temperatureDegree = document.querySelector('.temperature-degree');
     let locationTimezone = document.querySelector('.location-timezone');
+    let temperatureSection = document.querySelector(".temperature");
+    const temperatureSpan = document.querySelector(".temperature span")
 
 
     if (navigator.geolocation) {
@@ -27,8 +29,25 @@ window.addEventListener('load', () => {
                     temperatureDegree.textContent = temperature;
                     temperatureDescription.textContent = summary;
                     locationTimezone.textContent = data.timezone;
+                    // Formula for celsius
+                    let celsius = (temperature - 32) * (5 / 9);
+
+
                     // Set icon
                     setIcons(icon, document.querySelector(".icon"));
+
+                    // change temperatur to celcius
+                    temperatureSection.addEventListener('click', () => {
+                        if (temperatureSpan.textContent === "F") {
+                            temperatureSpan.textContent = "C";
+                            temperatureDegree.textContent = Math.floor(celsius);
+                        } else {
+                            temperatureSpan.textContent = "F";
+                            temperatureDegree.textContent = temperature;
+                        }
+
+                    });
+
 
                 });
 
